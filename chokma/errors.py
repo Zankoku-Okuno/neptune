15289145cb7c4c4e21d9373f48ab669138ee7716
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-06-10 17:13:03 -0400

    quick cleanup, add'l TODO items

--- chokma/errors.py
+++ chokma/errors.py
@@ -1,8 +1,18 @@
+_CODES = {
+	200: "OK",
+
+	404: "Not Found",
+	406: "Not Acceptable"
+
+	500: "Internal Server Error"
+}
+
 class HttpError(Exception):
 	def __init__(self, request, msg=None):
 		super().__init__(msg)
+		self.status = "%d %s" % (self.code, _CODES[self.code])
 		self.request = request
 
 
-class Http404(HttpError): pass
-class Http406(HttpError): pass
+class Http404(HttpError): code = 404
+class Http406(HttpError): code = 406
