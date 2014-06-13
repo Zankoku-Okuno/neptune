4b20f6457537e0682e292909c2592063756d45c2
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-06-13 11:44:55 -0400

    example of content negotiation

--- example/project.py
+++ example/project.py
@@ -1,6 +1,7 @@
+from os import path
 from chokma.http.pipeline import Endpoint, Route as URL, Resource as R, Renderer as View
 from chokma.http.routing import Slug
-from chokma import fs
+from chokma import fs, html
 from chokma.errors import HttpError
 from chokma.http.context import EmptyResponse
 
@@ -22,10 +23,26 @@ class FileR(R):
 class FileV(View):
     default_content_type = 'text/plain'
     def text(self, context, filepath):
-        if context.response.code == 201:
-            pass
-        else:
-            raise fs.Sendfile(filepath+'.txt')
+        raise fs.Sendfile(filepath+'.txt')
+
+    def html(self, context, filepath):
+        doc = html.Document()
+        doc.title = path.basename(filepath)
+        pars = []
+        buffer = []
+        with open(filepath+'.txt', 'rt') as fp:
+            for line in fp.readlines():
+                line = line.strip()
+                if line:
+                    buffer.append(line)
+                else:
+                    pars.append(' '.join(buffer))
+                    buffer = []
+        if buffer:
+            pars.append(' '.join(buffer))
+        del buffer
+        doc.append(*map(html.p, pars))
+        return html.render(doc)
 
 endpoints = (
     Endpoint('file', FileR(), FileV(),
