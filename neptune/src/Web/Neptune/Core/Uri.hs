2e5a87cdd765fd57e83734bf8b9afe53171a9b3b
Zankoku Okuno <zankoku.okuno@gmail.com>
2016-05-19 16:41:29 -0400

    abstracted ByteString out of the Uri definition

--- neptune/src/Web/Neptune/Core/Uri.hs
+++ neptune/src/Web/Neptune/Core/Uri.hs
@@ -1,5 +1,10 @@
-{-#LANGUAGE GADTs, ExistentialQuantification, TypeFamilies, PolyKinds, FlexibleInstances #-}
-module Web.Neptune.Core.Uri where
+{-#LANGUAGE GADTs, ExistentialQuantification, TypeFamilies, PolyKinds, FlexibleInstances, UndecidableInstances #-}
+module Web.Neptune.Core.Uri
+    ( Uri(..)
+    , Dispatch, fromList, DispatchM
+    , dispatch
+    , subapp
+    ) where
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
@@ -11,83 +16,84 @@ import Control.Monad.Writer
 type Bijection a b = (a -> b, b -> a)
 
 
-data Match a
-    = Match a
-    | NoMatch
-data Uri a where
-    Simple :: ByteString -> Uri () -- ^ recognize a 'Uri' that simply equals the given literal
-    Complex :: (Bijection ByteString (Match a)) -> Uri a -- ^ recognize a 'Uri' based on any algorithm, return true if it is a match
-    Prefix :: ByteString -> (Bijection ByteString (Match a)) -> Uri a -- ^ recognize a 'Uri' by stripping a prefix, then applying an algorithm as in 'Complex'
 
-addPrefix :: ByteString -> Uri a -> Uri a
+class Uri t where
+    data Matcher t :: * -> *
+    data ConcatError t
+
+    -- if 'x `overlaps` y == False', then for all 'a', 'isJust $ x `match` a' implies 'isNothing $ y `match` a'
+    match :: Matcher t r -> t -> Maybe r
+    -- overlaps is commutative
+    overlaps :: Matcher t r -> Matcher t r -> Bool
+
+data Dispatch t r = Dispatch_ [Matcher t r]
+type DispatchM t r = Writer [ConcatError t] (Dispatch t r)
+
+fromList :: (Uri uri, Traversable t) => t (Matcher uri r) -> Dispatch uri r
+fromList xs = Dispatch_ $ foldr (:) [] xs
+
+dispatch :: (Uri t) => Dispatch t r -> t -> Maybe r
+dispatch (Dispatch_ ds) it = asum $ map (`match` it) ds
+
+addAll :: (Uri t) => Dispatch t r -> [Matcher t r] -> DispatchM t r
+addAll old new = foldM add old new
+
+add :: (Uri t) => Dispatch t r -> Matcher t r -> DispatchM t r
+add (Dispatch_ old) new = Dispatch_ <$> go old
+    where
+    go [] = pure [new]
+    go (x:xs) = do
+        when (new `overlaps` x) $ tell [Overlap new x]
+        (x:) <$> go xs
+
+instance (Uri t) => Monoid (DispatchM t r) where
+    mempty = pure $ Dispatch_ []
+    mappend orig' new' = do
+       (Dispatch_ orig) <- orig'
+       (Dispatch_ new) <- new'
+       pure (Dispatch_ $ orig <> new) 
+
+
+
+
+class (Eq a, Monoid a) => Prefix a where
+    isPrefixOf :: a -> a -> Bool
+    stripPrefix :: a -> a -> Maybe a
+
+instance Prefix ByteString where
+    isPrefixOf = BS.isPrefixOf
+    stripPrefix = BS.stripPrefix
+
+data PrefixMatcher str a where
+    Simple :: str -> PrefixMatcher str () -- ^ recognize a uri that simply equals the given literal
+    Complex :: (Bijection str (Maybe a)) -> PrefixMatcher str a -- ^ recognize a uri based on any algorithm, return true if it is a match
+    Prefix :: str -> (Bijection str (Maybe a)) -> PrefixMatcher str a -- ^ recognize a uri by stripping a prefix, then applying an algorithm as in 'Complex'
+
+addPrefix :: (Prefix str) => str -> PrefixMatcher str a -> PrefixMatcher str a
 addPrefix prefix (Simple uri) = Simple (prefix <> uri)
 addPrefix prefix (Complex biject) = Prefix prefix biject
 addPrefix prefix (Prefix old biject) = Prefix (prefix <> old) biject
 
-match :: Uri a -> ByteString -> Match a
-match (Simple uri) candidate = Match ()
-match (Complex (recognize, _)) candidate = recognize candidate
-match (Prefix prefix (recognize, _)) candidate =
-    case BS.stripPrefix prefix candidate of
-        Nothing -> NoMatch
-        Just rest -> recognize rest
+
+instance (Prefix str) => Uri str where
+    data Matcher str r = forall a. DUri (PrefixMatcher str a) (a -> r)
+    data ConcatError str = forall r. Overlap (Matcher str r) (Matcher str r)
+
+    match (DUri (Simple uri) next) candidate = pure $ next ()
+    match (DUri (Complex (recognize, _)) next) candidate = next <$> recognize candidate
+    match (DUri (Prefix prefix (recognize, _)) next) candidate = next <$> (stripPrefix prefix candidate >>= recognize)
 
 
-
-class Dispatchable t where
-    data Dispatch t :: * -> *
-    type DispatchOn t
-    data ConcatError t
-
-    add :: Dispatches t r -> Dispatch t r -> DispatchesM t r
-    dispatch :: Dispatch t r -> DispatchOn t -> Maybe r
-
-data Dispatches t r = Dispatches [Dispatch t r]
-type DispatchesM t r = Writer [ConcatError t] (Dispatches t r)
-
-dispatches :: (Dispatchable t) => Dispatches t r -> DispatchOn t -> Maybe r
-dispatches (Dispatches ds) it = asum $ map (`dispatch` it) ds
-
-addAll :: (Dispatchable t) => Dispatches t r -> [Dispatch t r] -> DispatchesM t r
-addAll old new = foldM add old new
-
-instance (Dispatchable t) => Monoid (DispatchesM t r) where
-    mempty = pure $ Dispatches []
-    mappend orig' new' = do
-       (Dispatches orig) <- orig'
-       (Dispatches new) <- new'
-       pure (Dispatches $ orig <> new) 
-
-
-
-
-
-instance Dispatchable Uri where
-    data Dispatch Uri r = forall a. DUri (Uri a) (a -> r)
-    type DispatchOn Uri = ByteString
-    data ConcatError Uri = forall r. Overlap (Dispatch Uri r) (Dispatch Uri r)
-
-    dispatch (DUri uri next) str = case uri `match` str of
-        NoMatch -> Nothing
-        Match a -> Just $ next a
-
-    add (Dispatches old) new = Dispatches <$> go old
+    overlaps (DUri a _) (DUri b _) = check a b
         where
-        go [] = pure [new]
-        go (x:xs) = do
-            if isAmbiguous x new
-                then old <$ tell [Overlap new x]
-                else (x:) <$> go xs
-        isAmbiguous (DUri a _) (DUri b _) = check a b
-            where
-            check (Simple a) (Simple b) = a == b
-            check (Simple a) (Prefix b _) = b `BS.isPrefixOf` a
-            check (Prefix a _) (Simple b) = a `BS.isPrefixOf` b
-            check (Prefix a _) (Prefix b _) = a `BS.isPrefixOf` b || b `BS.isPrefixOf` a
-            check _ _ = False
+        check (Simple a) (Simple b) = a == b
+        check (Simple a) (Prefix b _) = b `isPrefixOf` a
+        check (Prefix a _) (Simple b) = a `isPrefixOf` b
+        check (Prefix a _) (Prefix b _) = a `isPrefixOf` b || b `isPrefixOf` a
+        check _ _ = False
 
 
-subapp :: Dispatches Uri r -> (ByteString, Dispatches Uri r) -> DispatchesM Uri r
-subapp old (prefix, Dispatches new) = pure old <> (pure . Dispatches) (addPrefix' prefix <$> new)
+subapp :: (Prefix str) => Dispatch str r -> (str, Dispatch str r) -> DispatchM str r
+subapp old (prefix, Dispatch_ new) = pure old <> (pure . Dispatch_) (addPrefix' prefix <$> new)
     where addPrefix' prefix (DUri it next) = DUri (addPrefix prefix it) next
 
